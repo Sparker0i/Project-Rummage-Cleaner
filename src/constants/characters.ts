c0c6f82ed3c7a25a4a79c9df0b40c73f1cc09fc9
Aaditya Menon <14982488+Sparker0i@users.noreply.github.com>
2025-06-22 01:38:37 +0000

    Characters to clean up

---
+++ src/constants/characters.ts
@@ -0,0 +1,21 @@
+export const INVISIBLE_CHARS: Record<string, string> = {
+  'Zero Width Space': '\u200B',
+  'Zero Width Non-Joiner': '\u200C',
+  'Zero Width Joiner': '\u200D',
+  'Byte Order Mark': '\uFEFF',
+  'Soft Hyphen': '\u00AD',
+  'Left-to-Right Mark': '\u200E',
+  'Right-to-Left Mark': '\u200F',
+  'Left-to-Right Embedding': '\u202A',
+  'Right-to-Left Embedding': '\u202B',
+  'Pop Directional Formatting': '\u202C',
+  'Left-to-Right Override': '\u202D',
+  'Right-to-Left Override': '\u202E',
+  'Right Single Quotation Mark': '\u2019',
+  'Left Double Quotation Mark': '\u201C',
+  'Right Double Quotation Mark': '\u201D',
+  'Horizontal Ellipsis': '\u2026',
+  'Em Dash': '\u2014',
+  'En Dash': '\u2013'
+};
+
